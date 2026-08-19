04bba7128e6f683b38fd81f652af5ec5e05b186d
farida <faridasobhy888@gmail.com>
2026-08-19 20:16:23 +0300

    added cancel booking fn

--- src/contollers/bookings.controller.ts
+++ src/contollers/bookings.controller.ts
@@ -1,47 +1,77 @@
 import type { Request, Response } from "express";
-import Booking from "../models/bookings.model.js"
-import ClassSession from "../models/temp-sessions.model.js"
+import Booking from "../models/bookings.model.js";
+import ClassSession from "../models/temp-sessions.model.js";
 
 const bookClass = async (req: Request, res: Response) => {
-    try{
-  const { sessionId } = req.body;
-  const memberId = req.user!.id;
-  const session= await ClassSession.findById(sessionId)
-  if(!session){
-    return res.status(404).json({
-        message :"Class session not found"
-    })
-  } 
-   if (new Date(session.timeSlot) < new Date()) {
+  try {
+    const { sessionId } = req.body;
+    const memberId = req.user!.id;
+    const session = await ClassSession.findById(sessionId);
+    if (!session) {
+      return res.status(404).json({
+        message: "Class session not found",
+      });
+    }
+    if (new Date(session.timeSlot) < new Date()) {
       return res.status(400).json({ message: "Session must be in the future" });
     }
-  const booked=await Booking.findOne({
-    member: memberId,
-  session: sessionId,
-  status: "booked"
-  })
-  if(booked){
-    return res.status(400).json({ message: "You already booked this session" });
-  }
-  const bookings=await Booking.find({ 
-  session: sessionId,
-  status: "booked"})
-  if(bookings.length>=session.capacity){
-    return res.status(400).json({ message: "This session is fully booked" });
-
-  }
-  const  booking=await Booking.create({
-    member: memberId,
-  session: sessionId
-  })
-  
+    const booked = await Booking.findOne({
+      member: memberId,
+      session: sessionId,
+      status: "booked",
+    });
+    if (booked) {
+      return res
+        .status(400)
+        .json({ message: "You already booked this session" });
+    }
+    const bookings = await Booking.find({
+      session: sessionId,
+      status: "booked",
+    });
+    if (bookings.length >= session.capacity) {
+      return res.status(400).json({ message: "This session is fully booked" });
+    }
+    const booking = await Booking.create({
+      member: memberId,
+      session: sessionId,
+    });
     return res.status(201).json(booking);
-  
-}
-catch(error){
-    return res.status(500).json({ message: "Error Booking" ,error});
-
-}  
+  } catch (error) {
+    return res.status(500).json({ message: "Error booking class", error });
+  }
 };
 
-export { bookClass };
+const cancelBooking = async (req: Request, res: Response) => {
+  try {
+    const { bookingId } = req.params;
+    const memberId = req.user!.id;
+
+    const booking = await Booking.findById(bookingId);
+    if (!booking) {
+      return res.status(404).json({
+        message: "Booking not found",
+      });
+    }
+    const session = await ClassSession.findById(booking.session);
+
+    if (session&&new Date(session.timeSlot) < new Date()) {
+      return res.status(400).json({ message: "Session must be in the future" });
+    }
+    if (booking.status === "cancelled") {
+      return res.status(400).json({
+        message: "This booking is already cancelled",
+      });
+    }
+    booking.status = "cancelled";
+    await booking.save();
+
+    return res.status(200).json(booking);
+  } catch (error) {
+    return res.status(500).json({ message: "Error cancelling booking", error });
+  }
+};
+
+
+
+export { bookClass, cancelBooking };
