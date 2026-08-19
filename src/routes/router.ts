8cc0a2269989c856d89647fea862ae38901dfc46
Roaa Hassan <roaa.hassanmoh@gmail.com>
2026-08-19 22:46:17 +0300

    classSesssion

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,5 +1,16 @@
 import { Router } from "express";
 
+import {
+    createClass,
+    getClasses,
+    updateClass,
+    deleteClass,
+    searchClasses
+} from "../controllers/classSession.controller.js";
 
 export const router = Router();
-
+router.post("/", createClass);
+router.get("/", getClasses);
+router.get("/search", searchClasses);
+router.put("/:id", updateClass);
+router.delete("/:id", deleteClass);
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import express from "express";
 import type { Application } from "express";
 import { specs } from "./config/swagger.js";
 import swaggerUI from "swagger-ui-express"
-import { router } from "./routes/router.js";
 import DBconfig from "./config/db.js";
+import router from "./routes/router";
 const app: Application = express();
 app.use(express.json());
 app.use('/',router)
@@ -15,3 +15,5 @@ app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(specs))
 app.listen(process.env.PORT ,()=>{
     console.log(`Server is running in port ${process.env.PORT}`)
 })
+app.use("/api/classes", router);
+
